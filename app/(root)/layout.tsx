9befde85e5d56a3c8aa972648cb7c2a101ca0167
Michael <michalkowal1989@gmail.com>
2025-01-22 22:25:19 +0100

    implement qstash workflow for user onboarding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import React, { ReactNode } from "react";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
@@ -8,12 +12,28 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   if (!session) redirect("/sign-in");
 
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // update only if user was not active today
+    const user = await db.select().from(users).where(eq(users.id, session?.user?.id)).limit(1);
+    const today = new Date().toISOString().slice(0, 10);
+
+    if(user[0].lastActivityDate === today) return;
+
+    // update last activity date 
+    // from user in database
+    // without time
+    await db
+      .update(users)
+      .set({ lastActivityDate: today})
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        <Header 
-          session={session}
-        />
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
--- database/schema.ts
+++ database/schema.ts
@@ -7,15 +7,15 @@ export const BORROW_STATUS_ENUM = pgEnum("borrow_status", ["BORROWED", "RETURNED
 
 export const users = pgTable("users", {
   id: uuid('id').notNull().primaryKey().defaultRandom().unique(),
-    fullName: varchar("full_name").notNull(),
-    email: text("email").notNull().unique(),
-    universityId: integer("university_id").notNull().unique(),
-    password: text("password").notNull(),
-    universityCard: text("university_card").notNull(),
-    status: STATUS_ENUM('status').default('PENDING'),
-    role: ROLE_ENUM('role').default('USER'),
-    lastActivityDate: date('last_activity_date').defaultNow(),
-    createdAt: timestamp("created_at", {
-        "withTimezone": true
-    }).defaultNow()
+  fullName: varchar("full_name").notNull(),
+  email: text("email").notNull().unique(),
+  universityId: integer("university_id").notNull().unique(),
+  password: text("password").notNull(),
+  universityCard: text("university_card").notNull(),
+  status: STATUS_ENUM('status').default('PENDING'),
+  role: ROLE_ENUM('role').default('USER'),
+  lastActivityDate: date('last_activity_date').defaultNow(),
+  createdAt: timestamp("created_at", {
+      "withTimezone": true
+  }).defaultNow()
 });
